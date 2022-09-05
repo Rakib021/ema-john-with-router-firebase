70d8b54f9b899db4212c43ca63b9384756251de1
Rakib <rakib@Rakibs-MacBook-Pro.local>
2022-09-05 20:04:18 +0600

    starting

--- src/hooks/useCart.js
+++ src/hooks/useCart.js
@@ -1,24 +1,49 @@
 import { useEffect, useState } from "react"
 import { getStoredCart } from "../utilities/fakedb";
 
-const useCart = (products) => {
-    const [cart, setCart] = useState([]);
-
-    useEffect( () =>{
+const useCart =(products)=>{
+    const [cart,setCart] = useState([]);
+    useEffect(()=>{
         const storedCart = getStoredCart();
         const savedCart = [];
         for(const id in storedCart){
-            const addedProduct = products.find(product => product.id === id);
+            const addedProduct = products.find(product => product.id ===id);
             if(addedProduct){
                 const quantity = storedCart[id];
-                addedProduct.quantity = quantity;
+                addedProduct.quantity =quantity;
                 savedCart.push(addedProduct);
             }
+
         }
         setCart(savedCart);
-    }, [products]);
-    
-    return [cart, setCart];
+    },[products]);
+    return [cart,setCart];
 }
-
 export default useCart;
+
+
+
+// import { useEffect, useState } from "react"
+// import { getStoredCart } from "../utilities/fakedb";
+
+// const useCart = (products) => {
+//     const [cart, setCart] = useState([]);
+
+//     useEffect( () =>{
+//         const storedCart = getStoredCart();
+//         const savedCart = [];
+//         for(const id in storedCart){
+//             const addedProduct = products.find(product => product.id === id);
+//             if(addedProduct){
+//                 const quantity = storedCart[id];
+//                 addedProduct.quantity = quantity;
+//                 savedCart.push(addedProduct);
+//             }
+//         }
+//         setCart(savedCart);
+//     }, [products]);
+    
+//     return [cart, setCart];
+// }
+
+// export default useCart;
--- src/hooks/useProducts.js
+++ src/hooks/useProducts.js
@@ -1,16 +1,29 @@
 import { useEffect, useState } from "react"
 
-const useProducts = () =>{
-    
-    const [products, setProducts] = useState([]);
-
-    useEffect( () =>{
-        fetch('products.json')
-        .then(res => res.json())
-        .then(data => setProducts(data));
-    }, []);
-
-    return [products, setProducts];
+const useProducts =()=>{
+    const [products,setProducts] = useState([]);
+    useEffect(()=>{
+        fetch('./products.json')
+        .then(res =>res.json())
+        .then(data => setProducts(data))
+    },[])
+    return [products,setProducts];
 }
-
 export default useProducts;
+
+// import { useEffect, useState } from "react"
+
+// const useProducts = () =>{
+    
+//     const [products, setProducts] = useState([]);
+
+//     useEffect( () =>{
+//         fetch('products.json')
+//         .then(res => res.json())
+//         .then(data => setProducts(data));
+//     }, []);
+
+//     return [products, setProducts];
+// }
+
+// export default useProducts;
